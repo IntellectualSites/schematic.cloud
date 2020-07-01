e16daa23d5572b7d74fc72a9f0c01b069db234e1
simpleauthority <jacob@algorithmjunkie.com>
2020-07-01 06:34:50 -0700

    github and seo stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,14 +9,14 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'SchematicWeb - Upload and Store Schematics',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Easily store your NBT Minecraft schematics in the cloud for later use',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
